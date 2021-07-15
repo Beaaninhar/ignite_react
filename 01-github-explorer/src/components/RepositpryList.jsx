57aef53a0db830cc52f77a51a408c724583fbf2a
Ana Beatriz <ana.beatriz@eurekalabs.com.br>
2021-07-15 19:04:23 -0300

    feito: aula -> Utilizando p useEffect

--- 01-github-explorer/src/components/RepositpryList.jsx
+++ 01-github-explorer/src/components/RepositpryList.jsx
@@ -1,6 +1,9 @@
 import { RepositpryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
+import { useEffect, useState } from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: "julia",
@@ -9,6 +12,14 @@ const repository = {
 }
 
 export function RepositpryList() {
+  const [repositories, setRepositories] = useState(0)
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>

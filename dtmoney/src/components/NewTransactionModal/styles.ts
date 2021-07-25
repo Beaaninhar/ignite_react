07f0c461f0f3a4e88a3d787035917b643b323304
ana.beatriz <ana.beatriz@m4u.com.br>
2021-07-25 10:54:49 -0300

    Feito: Aula -> Cores dos botoes

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken} from 'polished'
+import { darken, transparentize} from 'polished'
 
 export const Container = styled.form`
   h2 {
@@ -56,6 +56,12 @@ export const TransactionTypeContainer = styled.div`
 
 interface radioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
 }
 
 export const RadioBox = styled.button<radioBoxProps>`
@@ -64,7 +70,10 @@ export const RadioBox = styled.button<radioBoxProps>`
   border-radius: 0.25rem;
 
 
-  background: ${(props) => props.isActive ? 'red' : 'transoarent'};
+  background: ${(props) => props.isActive 
+  ? transparentize(0.9, colors[props.activeColor])
+  : 'transoarent'};
+  
   display: flex;
   align-items: center;
   justify-content: center;

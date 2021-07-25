5ae676896a989cbcaa0a5476923f1f0707e87a92
ana.beatriz <ana.beatriz@m4u.com.br>
2021-07-25 10:48:10 -0300

    Feito: Aula -> Funcionamento dos Botoes

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeIgm from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+
+
 import * as S from './styles'
 
 interface NewTransactionModalProps {
@@ -8,6 +13,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen} 
@@ -22,7 +29,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         >
           <img src={closeIgm} alt="Fchar modal" />
         </button>
-        
+
         <S.Container>
         <h2>Nova Transação</h2>
 
@@ -34,6 +41,28 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
           type="number" 
           placeholder="Valor"
         />
+
+      <S.TransactionTypeContainer>
+        <S.RadioBox
+          type="button"
+          onClick={() => setType('deposit')}
+          isActive={type === 'deposit'}
+        >
+          <img src={incomeImg} alt="Entrada" />
+          <span>Entrada</span>
+        </S.RadioBox>
+
+        <S.RadioBox
+          type="button"
+          onClick={() => setType('withdraw')}
+          isActive={type === 'withdraw'}
+
+
+        >
+          <img src={outcomeImg} alt="Saída" />
+          <span>Saída</span>
+        </S.RadioBox>
+      </S.TransactionTypeContainer>
 
         <input 
           placeholder="Categoria" 
